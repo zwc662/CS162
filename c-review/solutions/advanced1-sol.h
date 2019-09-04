cd42f3c62005a92c2329260cce4e2c4ae7e779f2
njriasan <njriasanovsky@berkeley.edu>
2019-09-03 18:16:57 -0700

    added the review session code

---
+++ c-review/solutions/advanced1-sol.h
@@ -0,0 +1,4 @@
+#ifndef ADVANCED1_H
+#define ADVANCED1_H
+extern int global;
+#endif
---
+++ c-review/solutions/advanced2-sol.c
@@ -0,0 +1,6 @@
+#include "advanced1-sol.h"
+#include "advanced2.h"
+
+void change_global () {
+    global = 15;
+}
